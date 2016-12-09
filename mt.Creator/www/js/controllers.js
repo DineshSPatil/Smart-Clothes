e7994991781164351c827124e04a20ac7f3fc034
Patil <dipatil@ptc.com>
2016-12-09 23:44:41 +0530

    Added mt.Creator folder

---
+++ mt.Creator/www/js/controllers.js
@@ -0,0 +1,83 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cartCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('cloudCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('menuCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $state) {
+
+	//$scope.userData = $ionicUser.details;
+
+    $scope.logout = function(){
+        //$ionicAuth.logout();
+        $state.go('login');
+    }
+
+}])
+   
+.controller('loginCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('signupCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams, $state) {
+
+	$scope.data = {
+		'name': '',
+		'email': '',
+		'password': ''
+	}
+
+	$scope.error='';
+
+	$scope.signup = function(){
+		
+		$scope.error='';
+
+		true.then(function(){
+			$state.go('menu.home');
+		}, function(err) {
+			var error_lookup = {
+				'required_email': 'Missing email field',
+                'required_password': 'Missing password field',
+                'conflict_email': 'A user has already signed up with that email',
+                'conflict_username': 'A user has already signed up with that username',
+                'invalid_email': 'The email did not pass validation'
+			}
+
+			$scope.error = error_lookup[err.details[0]];
+		});
+	}
+
+
+}])
+ 
